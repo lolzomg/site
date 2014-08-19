3eb59641da0ec9c9b167ecbe4747e8f921e56a2a
Evgeny <lolzomg@mail.ru>
2014-08-19 23:32:57 +0600

    ver 2

--- js/main.js
+++ js/main.js
@@ -1,109 +1,159 @@
-//$(window).load(function() {
-$(document).ready(function(){
-	
-	var currentPage = null;
-	
-	var emptyBox = $(".emptyBox");
-	
-	var backLink = $(".tileMenuBack");
-	
-	$(".tileMenuItem").delay(268).each(
-		 		function(i) {
-		 			$(this).delay(250*i).animate({top: 0}, 750);
-		 		}
-		 	);
-	
+var menuGlob = null;
+var openedPage = null;
 
+$(document).ready(function() {
+	//onLoadIntro();
+	menuGlob = $(".menu");
+	//var subMenu = $(".subMenu");
 	
-	$(".tileMenuItem").click(function(){
-		currentPage = $(this);
+	buildMenu(menuGlob, true);
 		
-		
-		//emptyBox.insertBefore(currentPage).css({"display":"table"});
-		
-		//
-		
-		var elementPosIndex = currentPage.index();
-		var elementWidthOrig = parseFloat(currentPage.css("width"));
-		
-		currentPage.css({"z-index" : "9"});		
-		currentPage.children(".tileIcon").children().animate({opacity : "0"},600);
-		currentPage.animate({width: "100%", left: 0},700, function(){
-			
-			
-			
-			backLink.removeClass("hidden").animate({bottom : 0},550);
-			currentPage.removeClass("collapsed");
-			
-			//
-			
-			currentPage.children(".tileIcon").children().animate({top: "-25%", "font-size" : "1em", opacity : "0"},600,function(){
-				//$(this).removeClass("collapsed")
-				currentPage.children(".tileIcon").animate({width : "15%"},300);
-				$(this).children("i").css({"display":"inline"});
-				
-				currentPage.children(".tileIcon").children().animate({opacity : "1"},600);
-				
-			});
-			
-			
-			currentPage.children(".tileContent").removeClass("hidden");
-			
-			currentPage.children(".tileContent").children(".tileSubMenu").children("li").each(function(i){
-				$(this).delay(i*150).animate({bottom : "0%"},350);
-			});
-		});
-		
-		
+	var item = menuGlob.children(".item");
+	
+	item.click(function(){
+		openedPage = $(this);
+		itemMenuClick($(this));
+		buildMenu($(this).children(".subMenu"), false);
 	});
 	
-	backLink.click(function(){
-		
-		
-		var elementPosIndex = currentPage.index();
-		
-		currentPage.children(".tileContent").children(".tileSubMenu").children("li").each(function(i){
-				$(this).delay(i*150).animate({bottom : "-100%"},350, function(){
-					
-					if(i === 1) {
-						//currentPage.children(".tileIcon").addClass("collapsed");
-					}
-					
-					if(i === 3) {
-						
-						//currentPage.children(".tileIcon").addClass("collapsed");
-						
-						currentPage.css({"display":"table"});
-						
-						currentPage.children(".tileContent").addClass("hidden");
-						$("span.descr").children("i").css({"display":"none"});
-						
-						//currentPage.children(".tileIcon").children().children("i").css({"display":"none"});
-						currentPage.children(".tileIcon").children("i").animate({top: "-25%", "font-size" : "64px", opacity : "0"},600,function(){
-							//$(this).removeClass("collapsed")
-							currentPage.children(".tileIcon").animate({width : "100%"},300);
-							
-							
-							currentPage.children(".tileIcon").children().animate({opacity : "1"},600);
-				
-						});
-						
-						currentPage.animate({width: 196, left: 196*elementPosIndex},700, function(){
-							currentPage.removeAttr("style");
-							currentPage.css({top : 0});
-							currentPage.addClass("collapsed");
-							currentPage.css({"z-index" : "1"});
-						});
-					}
-				});
-		});
-		
-		backLink.animate({bottom : -201},550, function(){
-				backLink.addClass("hidden");
-				
-		});
-		
-		
+	var backMenu = $(".backLink");
+	backMenu.click(function(){
+		backLinkClick();
 	});
 	
 });
+
+function onLoadIntro() {
+	/*var $dots = $(".introDot");
+
+	for (var i = 1; i < 4; ++i) {
+		(function(n) {
+			setTimeout(function() {
+				$dots.append(".");
+			}, 1000*n);
+		}(i));
+	}
+*/
+	var intro = $(".intro");
+	var menu = $(".menu");
+	
+	intro.animate({"opacity" : "0"},1500, function(){
+		$(this).css({"display" : "none"});
+		menu.css({"display" : "block"});
+		
+	});
+
+
+}
+
+function buildMenu(menu, b) {
+	//var menu = $(".menu");	
+	
+	var item = menu.children();
+	var itemWidth = menu.width() / item.length;
+	
+	item.each(function(i){
+		$(this).css({"width" : itemWidth, "left" : i*itemWidth});
+	});
+	
+	if(b) animateMenuDrop(menu);
+	if(!b) animateMenuPop(menu, false);
+}
+
+function animateMenuDrop(menu) {
+	//var menu = $(".menu");
+	var item = menu.children();
+		
+	item.each(function(i){
+		$(this).delay((i+1)*300).animate({"top" : "0"},600)		
+	});
+}
+
+function animateMenuPop(menu, reverse) {
+	var item = menu.find(".subItem");		
+	if(reverse===true) {
+		
+		item.each(function(i){
+			$(this).delay((i+1)*300).animate({"bottom" : "-100%"},600, function(){
+				/*if(i===3) {
+					var indexItem = openedPage.index();
+					openedPage.css({"z-index" : ""}).animate({"width" : 200, left : indexItem * 200}, function(){});
+				}*/
+			})
+		});
+	};
+		
+	if(reverse===false) {
+		item.each(function(i){
+			$(this).delay((i+1)*300).animate({"bottom" : "0"},600)
+		});
+	};
+	
+	
+	
+	//if(menu.parent().hasClass("page-about")===true)	enlargeYourMenu(menuGlob);
+}
+
+function enlargeYourMenu(menu) {
+	menu.animate({"height" : 800},600);
+	backLinkUpdate(800);
+}
+
+function decreaseYourMenu(menu) {
+	menu.animate({"height" : 400},600);
+	backLinkUpdate(400);
+}
+
+function itemMenuClick(owner){
+	
+	owner.find(".textBlock, .subMenu").css({opacity : 1});
+	
+	owner.css({"z-index" : "99"}).animate({"left" : 0, "width" : "100%"}, function() {
+		squezeLabel();
+		if(owner.hasClass("page-about")===true)	enlargeYourMenu(menuGlob);
+		//backLinkUpdate();
+	});
+	
+	function squezeLabel() {
+		owner.children(".label").children(".labelContent").children("i").animate({"font-size" : "3em"}, 400);
+		owner.children(".label").animate({"height" : "20%", "font-size" : "3.5em"}, 400);
+	}
+}
+
+function backLinkUpdate(amount) {
+	var backLink = $(".backLink");
+	
+	backLink.animate({height : amount},600);
+}
+
+function backLinkClick() {
+	
+	openedPage.find(".textBlock, .subMenu").animate({opacity : 0},300, function(){
+		
+		decreaseYourMenu(menuGlob);
+		
+		
+		var indexItem = openedPage.index();
+		openedPage.css({"z-index" : ""}).animate({"width" : 200, left : indexItem * 200}, function(){
+			deSquezeLabel(openedPage);
+		});
+	});
+	
+	animateMenuPop(openedPage, true);
+	//closeMenuItem(openedPage);
+		
+	
+	
+	function deSquezeLabel(owner) {
+		owner.children(".label").children(".labelContent").children("i").animate({"font-size" : "4em"}, 400);
+		owner.children(".label").animate({"height" : "100%", "font-size" : "5em"}, 400);
+	}
+	
+	
+	
+	
+}
+
+function closeMenuItem(item) {
+	
+}
